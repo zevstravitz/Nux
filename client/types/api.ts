5934c5a8edd80c906ab12dfb94a078d84b6ddaf1
Zev Stravitz <zevstravitz@verizon.net>
2020-04-01 00:30:39 -0400

    more accurate types

--- client/types/api.ts
+++ client/types/api.ts
@@ -15,13 +15,46 @@ export interface ISubject {
   path: string
 }
 
-export interface IClass {
+export interface IPreviewClass {
+  abbv: string,
+  name: string,
+  path: string
+}
+
+export interface IClass1 {
   id: string,
   name: string,
   topic: string,
-  section: 20,
+  section: string,
   instructor: string[],
   path: string,
   meeting_time: string[],
   overview_of_class: string
+}
+
+export interface IClass2 {
+  id: string,
+  lmod: string,
+  name: string,
+  title: string,
+  topic: string,
+  class_mtg_info: {
+    meet_t: string,
+    meet_l: string
+  }[],
+  instructor?: string[],
+  path?: string,
+  meeting_time?: string[],
+  descriptions?: {
+    name: string,
+    value: string
+  }[],
+  instructors?: {
+    instructor_name: string,
+    instructor_phone: string,
+    instructor_addr: string
+  }[],
+  enrl_requirement?: string,
+  class_attributes?: string
+
 }
