6bcc6d5a5062ce527c76062d7ca8be8c6379a527
Zev Stravitz <zevstravitz@verizon.net>
2020-04-01 00:09:48 -0400

    updated types

--- client/types/api.ts
+++ client/types/api.ts
@@ -3,3 +3,25 @@ export interface ITerm {
     term: string,
     acadyear: string
 }
+
+export interface ISchool {
+  id: string,
+  name: string
+}
+
+export interface ISubject {
+  abbv: string,
+  name: string,
+  path: string
+}
+
+export interface IClass {
+  id: string,
+  name: string,
+  topic: string,
+  section: 20,
+  instructor: string[],
+  path: string,
+  meeting_time: string[],
+  overview_of_class: string
+}
